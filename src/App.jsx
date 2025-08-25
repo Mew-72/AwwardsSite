d67b363ed6e0bef7badcb7411be113ab922814e5
Mayank Kumar <178730657+Mew-72@users.noreply.github.com>
2025-08-25 20:41:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => {
+    return <>
+        <div className={"border-2 border-blue-500 p-4 m-4 text-center text-xl font-bold"}>
+            Hello, React!
+        </div>
+    </>
+}
+export default App;
