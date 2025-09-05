4bb65dc4d29e0cb2bc0e51822123173ff017cdbf
Mayank Kumar <178730657+Mew-72@users.noreply.github.com>
2025-09-05 20:11:52 +0530

    completed hero section and message section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import NavBar from "./components/NavBar.jsx";
+import HeroSection from "./sections/HeroSection.jsx";
+import { gsap, ScrollTrigger, ScrollSmoother } from "gsap/all";
+import MessageSection from "./sections/MessageSection.jsx";
+import FlavorSection from "./sections/FlavorSection.jsx";
+import { useGSAP } from "@gsap/react";
+import { use } from "react";
+import NutritionSection from "./sections/NutritionSection.jsx";
+import BenefitSection from "./sections/BenefitSection.jsx";
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
-    return <>
-        <div className={"border-2 border-blue-500 p-4 m-4 text-center text-xl font-bold"}>
-            Hello, React!
-        </div>
-    </>
+    useGSAP(() => {
+        ScrollSmoother.create({
+            smooth: 3,
+            effects: true,
+            // normalizeScroll: true,
+        });
+    });
+    return (
+        <main>
+            <NavBar />
+            <div id="smooth-wrapper">
+                <div id="smooth-content">
+                    <HeroSection />
+                    <MessageSection />
+                    <FlavorSection />
+                    <NutritionSection />
+                    <BenefitSection />
+                    <div>
+                        <h1 className="text-9xl text-center py-20">End of the Page</h1>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
 }
 export default App;
